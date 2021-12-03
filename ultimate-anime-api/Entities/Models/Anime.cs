1b3fd9410ba5b1107bc226027fdbe596aac9adbd
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-12-03 21:42:31 +0100

    Project Entities with Models

---
+++ ultimate-anime-api/Entities/Models/Anime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Anime
+    {
+        [Column("AnimeId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Anime name is a required field.")]
+        [MaxLength(50, ErrorMessage = "Maximum length for the Name is 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Number of episodes is required field.")]
+        public int Episodes { get; set; }
+
+        [Required(ErrorMessage = "ReleaseDate is a required field.")]
+        public DateTime ReleaseDate { get; set; }
+
+        [ForeignKey(nameof(Studio))]
+        public Guid StudioId { get; set; }
+        public Studio Studio { get; set; }
+    }
+}
---
+++ ultimate-anime-api/Entities/Models/Studio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Studio
+    {
+        [Column("StudioId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Studio name is required field.")]
+        [MaxLength(50, ErrorMessage = "Maximum length for the Name is 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required field.")]
+        [MaxLength(50, ErrorMessage = "Maximum length for the Addres is 50 characters.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Country is required field.")]
+        [MaxLength(50, ErrorMessage = "Maximum length for the Country is 20 characters.")]
+        public string Country { get; set; }
+
+        public ICollection<Anime> Animes { get; set; }
+    }
+}
