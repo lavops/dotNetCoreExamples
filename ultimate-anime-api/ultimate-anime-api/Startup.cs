e8ddb91e45793d8a5d7a5f838433e0128b923899
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-12-03 20:45:28 +0100

    Logger service implementation

--- ultimate-anime-api/ultimate-anime-api/Startup.cs
+++ ultimate-anime-api/ultimate-anime-api/Startup.cs
@@ -8,8 +8,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ultimate_anime_api.Extensions;
@@ -20,6 +22,7 @@ namespace ultimate_anime_api
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -30,6 +33,7 @@ namespace ultimate_anime_api
         {
             services.ConfigureCors();
             services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
