3f4ad4cbfe12ca8b79fcfb73ad90db57131a991c
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-12-04 11:02:58 +0100

    Repository User Interfaces and Classes

---
+++ ultimate-anime-api/Repository/AnimeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AnimeRepository : RepositoryBase<Anime>, IAnimeRepository
+    {
+        public AnimeRepository(RepositoryContext repositoryContext): base(repositoryContext)
+        {
+
+        }
+    }
+}
---
+++ ultimate-anime-api/Repository/StudioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class StudioRepository : RepositoryBase<Studio>, IStudioRepository
+    {
+        public StudioRepository(RepositoryContext repositoryContext): base(repositoryContext)
+        {
+
+        }
+    }
+}
